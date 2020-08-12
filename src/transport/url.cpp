590bb6e780616579f3da4030193fe2877db20131
ousttrue <ousttrue@gmail.com>
2020-08-12 11:55:09 +0900

    openSocket

--- src/transport/url.cpp
+++ src/transport/url.cpp
@@ -1,4 +1,5 @@
 #include "fm.h"
+#include "gc_helper.h"
 #include "file.h"
 #include "transport/url.h"
 #include "indep.h"
@@ -87,23 +88,16 @@ static std::string_view DefaultFile(int scheme)
     return "";
 }
 
-int openSocket(const char *hostname,
-               const char *remoteport_name, unsigned short remoteport_num)
+int openSocket4(const char *hostname,
+                const char *remoteport_name, unsigned short remoteport_num)
 {
     int sock = -1;
-#ifdef INET6
-    int *af;
-    struct addrinfo hints, *res0, *res;
-    int error;
-    char *hname;
-#else  /* not INET6 */
     struct sockaddr_in hostaddr;
     struct hostent *entry;
     struct protoent *proto;
     unsigned short s_port;
     int a1, a2, a3, a4;
     unsigned long adr;
-#endif /* not INET6 */
     MySignalHandler prevtrap = NULL;
 
     if (fmInitialized)
@@ -115,16 +109,107 @@ int openSocket(const char *hostname,
 
     if (hostname == NULL)
     {
-#ifdef SOCK_DEBUG
-        sock_log("openSocket() failed. reason: Bad hostname \"%s\"\n",
-                 hostname);
-#endif
         return -1;
     }
 
     auto success = TrapJmp([&] {
+        s_port = htons(remoteport_num);
+        bzero((char *)&hostaddr, sizeof(struct sockaddr_in));
+        if ((proto = getprotobyname("tcp")) == NULL)
+        {
+            /* protocol number of TCP is 6 */
+            proto = New(struct protoent);
+            proto->p_proto = 6;
+        }
+        if ((sock = socket(AF_INET, SOCK_STREAM, proto->p_proto)) < 0)
+        {
+            return false;
+        }
+        regexCompile("^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$", 0);
+        if (regexMatch(const_cast<char*>(hostname), -1, 1))
+        {
+            sscanf(hostname, "%d.%d.%d.%d", &a1, &a2, &a3, &a4);
+            adr = htonl((a1 << 24) | (a2 << 16) | (a3 << 8) | a4);
+            bcopy((void *)&adr, (void *)&hostaddr.sin_addr, sizeof(long));
+            hostaddr.sin_family = AF_INET;
+            hostaddr.sin_port = s_port;
+            if (fmInitialized)
+            {
+                message(Sprintf("Connecting to %s", hostname)->ptr, 0, 0);
+                refresh();
+            }
+            if (connect(sock, (struct sockaddr *)&hostaddr,
+                        sizeof(struct sockaddr_in)) < 0)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            char **h_addr_list;
+            int result = -1;
+            if (fmInitialized)
+            {
+                message(Sprintf("Performing hostname lookup on %s", hostname)->ptr,
+                        0, 0);
+                refresh();
+            }
+            if ((entry = gethostbyname(hostname)) == NULL)
+            {
+                return false;
+            }
+            hostaddr.sin_family = AF_INET;
+            hostaddr.sin_port = s_port;
+            for (h_addr_list = entry->h_addr_list; *h_addr_list; h_addr_list++)
+            {
+                bcopy((void *)h_addr_list[0], (void *)&hostaddr.sin_addr,
+                      entry->h_length);
+                if (fmInitialized)
+                {
+                    message(Sprintf("Connecting to %s", hostname)->ptr, 0, 0);
+                    refresh();
+                }
+                if ((result = connect(sock, (struct sockaddr *)&hostaddr,
+                                      sizeof(struct sockaddr_in))) == 0)
+                {
+                    break;
+                }
+            }
+            if (result < 0)
+            {
+                return false;
+            }
+        }
 
-#ifdef INET6
+        return true;
+    });
+
+    return success ? sock : -1;
+}
+
+int openSocket6(const char *hostname,
+                const char *remoteport_name, unsigned short remoteport_num)
+{
+    int sock = -1;
+    int *af;
+    struct addrinfo hints, *res0, *res;
+    int error;
+    char *hname;
+    MySignalHandler prevtrap = NULL;
+
+    if (fmInitialized)
+    {
+        /* FIXME: gettextize? */
+        message(Sprintf("Opening socket...")->ptr, 0, 0);
+        refresh();
+    }
+
+    if (hostname == NULL)
+    {
+        return -1;
+    }
+
+    auto success = TrapJmp([&] {
         /* rfc2732 compliance */
         hname = const_cast<char *>(hostname);
         if (hname != NULL && hname[0] == '[' && hname[strlen(hname) - 1] == ']')
@@ -192,104 +277,17 @@ int openSocket(const char *hostname,
             freeaddrinfo(res0);
             break;
         }
-#else /* not INET6 */
-        s_port = htons(remoteport_num);
-        bzero((char *)&hostaddr, sizeof(struct sockaddr_in));
-        if ((proto = getprotobyname("tcp")) == NULL)
-        {
-            /* protocol number of TCP is 6 */
-            proto = New(struct protoent);
-            proto->p_proto = 6;
-        }
-        if ((sock = socket(AF_INET, SOCK_STREAM, proto->p_proto)) < 0)
-        {
-#ifdef SOCK_DEBUG
-            sock_log("openSocket: socket() failed. reason: %s\n", strerror(errno));
-#endif
-            goto error;
-        }
-        regexCompile("^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$", 0);
-        if (regexMatch(hostname, -1, 1))
-        {
-            sscanf(hostname, "%d.%d.%d.%d", &a1, &a2, &a3, &a4);
-            adr = htonl((a1 << 24) | (a2 << 16) | (a3 << 8) | a4);
-            bcopy((void *)&adr, (void *)&hostaddr.sin_addr, sizeof(long));
-            hostaddr.sin_family = AF_INET;
-            hostaddr.sin_port = s_port;
-            if (fmInitialized)
-            {
-                message(Sprintf("Connecting to %s", hostname)->ptr, 0, 0);
-                refresh();
-            }
-            if (connect(sock, (struct sockaddr *)&hostaddr,
-                        sizeof(struct sockaddr_in)) < 0)
-            {
-#ifdef SOCK_DEBUG
-                sock_log("openSocket: connect() failed. reason: %s\n",
-                         strerror(errno));
-#endif
-                goto error;
-            }
-        }
-        else
-        {
-            char **h_addr_list;
-            int result = -1;
-            if (fmInitialized)
-            {
-                message(Sprintf("Performing hostname lookup on %s", hostname)->ptr,
-                        0, 0);
-                refresh();
-            }
-            if ((entry = gethostbyname(hostname)) == NULL)
-            {
-#ifdef SOCK_DEBUG
-                sock_log("openSocket: gethostbyname() failed. reason: %s\n",
-                         strerror(errno));
-#endif
-                goto error;
-            }
-            hostaddr.sin_family = AF_INET;
-            hostaddr.sin_port = s_port;
-            for (h_addr_list = entry->h_addr_list; *h_addr_list; h_addr_list++)
-            {
-                bcopy((void *)h_addr_list[0], (void *)&hostaddr.sin_addr,
-                      entry->h_length);
-#ifdef SOCK_DEBUG
-                adr = ntohl(*(long *)&hostaddr.sin_addr);
-                sock_log("openSocket: connecting %d.%d.%d.%d\n",
-                         (adr >> 24) & 0xff,
-                         (adr >> 16) & 0xff, (adr >> 8) & 0xff, adr & 0xff);
-#endif
-                if (fmInitialized)
-                {
-                    message(Sprintf("Connecting to %s", hostname)->ptr, 0, 0);
-                    refresh();
-                }
-                if ((result = connect(sock, (struct sockaddr *)&hostaddr,
-                                      sizeof(struct sockaddr_in))) == 0)
-                {
-                    break;
-                }
-#ifdef SOCK_DEBUG
-                else
-                {
-                    sock_log("openSocket: connect() failed. reason: %s\n",
-                             strerror(errno));
-                }
-#endif
-            }
-            if (result < 0)
-            {
-                goto error;
-            }
-        }
-#endif /* not INET6 */
 
         return true;
     });
 
     return success ? sock : -1;
+}
+
+int openSocket(const char *hostname,
+               const char *remoteport_name, unsigned short remoteport_num)
+{
+    return openSocket6(hostname, remoteport_name, remoteport_num);
 }
 
 enum SpaceMode
