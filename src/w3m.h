d34b9192b45e62cadf93fc2be0ceaf57a39f1266
ousttrue <ousttrue@gmail.com>
2020-08-20 00:57:50 +0900

    rc

--- src/w3m.h
+++ src/w3m.h
@@ -76,7 +76,7 @@ public:
 
     // rc.cpp
     //
-    // Display Settings
+    // Display
     //
     int Tabstop = 8;
     int IndentIncr = 4;
@@ -113,6 +113,134 @@ public:
     bool show_srch_str = true;
     bool label_topline = false;
     bool nextpage_topline = false;
+
+    //
+    // Color
+    //
+    bool useColor = true;
+    int basic_color = 8;  /* don't change */
+    int anchor_color = 4; /* blue  */
+    int image_color = 2;  /* green */
+    int form_color = 1;   /* red   */
+    int mark_color = 6;   /* cyan */
+    int bg_color = 8;     /* don't change */
+    bool useActiveColor = false;
+    int active_color = 6; /* cyan */
+    bool useVisitedColor = false;
+    int visited_color = 5; /* magenta  */
+
+    //
+    // Miscellaneous
+    //
+    int PagerMax = 10000;
+    bool UseHistory = true;
+    int URLHistSize = 100;
+    bool SaveURLHist = true;
+    bool confirm_on_quit = true;
+    int close_tab_back = false;
+    bool use_mark = false;
+    bool emacs_like_lineedit = false;
+    bool vi_prec_num = false;
+    bool MarkAllPages = false;
+    bool WrapDefault = false;
+    bool IgnoreCase = true;
+    bool use_mouse = true;
+    bool reverse_mouse = false;
+    bool relative_wheel_scroll = false;
+    int relative_wheel_scroll_ratio = 30;
+    int fixed_wheel_scroll_count = 5;
+    bool clear_buffer = true;
+    bool DecodeCTE = false;
+    bool AutoUncompress = false;
+    bool PreserveTimestamp = true;
+    std::string keymap_file = KEYMAP_FILE;
+
+    //
+    // Directory
+    //
+    std::string document_root;
+    std::string personal_document_root;
+    std::string cgi_bin;
+    std::string index_file;
+
+    //
+    // External Program
+    //
+    std::string mimetypes_files = USER_MIMETYPES ", " SYS_MIMETYPES;
+    std::string mailcap_files = USER_MAILCAP ", " SYS_MAILCAP;
+    std::string urimethodmap_files = USER_URIMETHODMAP ", " SYS_URIMETHODMAP;
+    std::string Editor = DEF_EDITOR;
+    MailtoOption MailtoOptions = MAILTO_OPTIONS_IGNORE;
+    std::string Mailer = DEF_MAILER;
+    std::string ExtBrowser = DEF_EXT_BROWSER;
+    std::string ExtBrowser2;
+    std::string ExtBrowser3;
+    bool BackgroundExtViewer = true;
+    bool use_lessopen = false;
+
+    //
+    // Network
+    //
+    std::string passwd_file = PASSWD_FILE;
+    bool disable_secret_security_check = false;
+    std::string ftppasswd;
+    bool ftppass_hostnamegen = true;
+    std::string pre_form_file = PRE_FORM_FILE;
+    std::string UserAgent;
+    bool NoSendReferer = false;
+    std::string AcceptLang;
+    std::string AcceptEncoding;
+    std::string AcceptMedia;
+    bool ArgvIsURL = false;
+    bool retryAsHttp = true;
+    DefaultUrlTypes DefaultURLString = DEFAULT_URL_EMPTY;
+    int FollowRedirection = 10;
+    bool MetaRefresh = false;
+    int DNS_order = DNS_ORDER_UNSPEC;
+
+    //
+    // Proxy
+    //
+    bool use_proxy = true;
+    std::string HTTP_proxy;
+    std::string HTTPS_proxy;
+    std::string FTP_proxy;
+    char *NO_proxy = nullptr;
+    bool NOproxy_netaddr = true;
+    bool NoCache = false;
+
+    //
+    // SSL
+    //
+    std::string ssl_forbid_method;
+    bool ssl_verify_server = false;
+    std::string ssl_cert_file;
+    std::string ssl_key_file;
+    std::string ssl_ca_path;
+    std::string ssl_ca_file;
+
+    //
+    // Cookie
+    //
+    bool use_cookie = false;
+    bool show_cookie = true;
+    bool accept_cookie = false;
+    AcceptBadCookieTypes accept_bad_cookie = ACCEPT_BAD_COOKIE_DISCARD;
+    std::string cookie_reject_domains;
+    std::string cookie_accept_domains;
+    std::string cookie_avoid_wrong_number_of_dots;
+
+    //
+    // Charset
+    //
+    CharacterEncodingScheme DisplayCharset = DISPLAY_CHARSET;
+    CharacterEncodingScheme DocumentCharset = DOCUMENT_CHARSET;
+    CharacterEncodingScheme SystemCharset = SYSTEM_CHARSET;
+    bool FollowLocale = true;
+    bool ExtHalfdump = false;
+    bool SearchConv = true;
+    bool SimplePreserveSpace = false;
+
     //
     //
     //
@@ -122,74 +250,24 @@ public:
     std::string CurrentDir;
     TextList *fileToDelete = nullptr;
     bool fmInitialized = false;
-    std::string document_root;
-    std::string personal_document_root;
-    std::string cgi_bin;
-    std::string index_file;
     std::string CurrentCmdData;
-    bool confirm_on_quit = true;
-    bool use_mark = false;
-    bool emacs_like_lineedit = false;
-    bool vi_prec_num = false;
-    bool retryAsHttp = true;
-    std::string Editor = DEF_EDITOR;
-    std::string Mailer = DEF_MAILER;
-    MailtoOption MailtoOptions = MAILTO_OPTIONS_IGNORE;
-    std::string ExtBrowser = DEF_EXT_BROWSER;
-    std::string ExtBrowser2;
-    std::string ExtBrowser3;
-    bool BackgroundExtViewer = true;
-    bool disable_secret_security_check = false;
-    std::string passwd_file = PASSWD_FILE;
-    std::string pre_form_file = PRE_FORM_FILE;
-    std::string ftppasswd;
-    bool ftppass_hostnamegen = true;
+
+
     bool do_download = false;
     std::string image_source;
-    std::string UserAgent;
-    bool NoSendReferer = false;
-    std::string AcceptLang;
-    std::string AcceptEncoding;
-    std::string AcceptMedia;
-    bool IgnoreCase = true;
     bool WrapSearch = false;
-    DefaultUrlTypes DefaultURLString = DEFAULT_URL_EMPTY;
-    bool MarkAllPages = false;
-    std::string mailcap_files = USER_MAILCAP ", " SYS_MAILCAP;
-    std::string mimetypes_files = USER_MIMETYPES ", " SYS_MIMETYPES;
-    std::string urimethodmap_files = USER_URIMETHODMAP ", " SYS_URIMETHODMAP;
-    bool SearchConv = true;
-    bool SimplePreserveSpace = false;
     bool no_rc_dir = false;
     std::string rc_dir;
-    bool reverse_mouse = false;
-    bool relative_wheel_scroll = false;
-    int fixed_wheel_scroll_count = 5;
-    int relative_wheel_scroll_ratio = 30;
     std::string tmp_dir;
     bool default_use_cookie = true;
-    bool show_cookie = true;
-    AcceptBadCookieTypes accept_bad_cookie = ACCEPT_BAD_COOKIE_DISCARD;
-    std::string cookie_reject_domains;
-    std::string cookie_accept_domains;
-    std::string cookie_avoid_wrong_number_of_dots;
     TextList *Cookie_reject_domains = nullptr;
     TextList *Cookie_accept_domains = nullptr;
     TextList *Cookie_avoid_wrong_number_of_dots_domains = nullptr;
     bool is_redisplay = false;
-    bool clear_buffer = true;
-    bool use_lessopen = false;
-    std::string keymap_file = KEYMAP_FILE;
-    int FollowRedirection = 10;
 
     // ssl
-    bool ssl_verify_server = false;
-    std::string ssl_cert_file;
-    std::string ssl_key_file;
-    std::string ssl_ca_path;
-    std::string ssl_ca_file;
+    
     bool ssl_path_modified = false;
-    std::string ssl_forbid_method;
 
     // files settings
     std::string config_file;
@@ -201,63 +279,27 @@ public:
     // http
     bool override_content_type = false;
     std::string header_string;
-    bool use_cookie = false;
-    bool accept_cookie = false;
 
     // character encoding
-    bool FollowLocale = true;
     CharacterEncodingScheme InnerCharset = WC_CES_WTF; /* Don't change */
-    CharacterEncodingScheme DisplayCharset = DISPLAY_CHARSET;
-    CharacterEncodingScheme DocumentCharset = DOCUMENT_CHARSET;
-    CharacterEncodingScheme SystemCharset = SYSTEM_CHARSET;
     CharacterEncodingScheme BookmarkCharset = SYSTEM_CHARSET;
     bool UseContentCharset = true;
-    bool ExtHalfdump = false;
 
     // proxy
-    bool use_proxy = true;
     TextList *NO_proxy_domains = nullptr;
-    std::string HTTP_proxy;
     URL HTTP_proxy_parsed;
-    std::string HTTPS_proxy;
     URL HTTPS_proxy_parsed;
-    std::string FTP_proxy;
     URL FTP_proxy_parsed;
-    char *NO_proxy = nullptr;
-    bool NOproxy_netaddr = true;
-    int DNS_order = DNS_ORDER_UNSPEC;
-    bool NoCache = false;
 
     // frontend
 
     bool ShowEffect = true;
     bool PermitSaveToPipe = false;
-    bool DecodeCTE = false;
-    bool AutoUncompress = false;
-    bool PreserveTimestamp = true;
-    bool ArgvIsURL = false;
-    bool MetaRefresh = false;
     bool QuietMessage = false;
     bool TrapSignal = true;
-    int close_tab_back = false;
-
-    bool useColor = true;
-    int basic_color = 8;  /* don't change */
-    int anchor_color = 4; /* blue  */
-    int image_color = 2;  /* green */
-    int form_color = 1;   /* red   */
-    int bg_color = 8;     /* don't change */
-    int mark_color = 6;   /* cyan */
-    bool useActiveColor = false;
-    int active_color = 6; /* cyan */
-    bool useVisitedColor = false;
-    int visited_color = 5; /* magenta  */
 
     // Maximum line kept as pager
-    int PagerMax = 10000;
     bool SearchHeader = false;
-    bool WrapDefault = false;
-    bool use_mouse = true;
     bool squeezeBlankLine = false;
     bool Do_not_use_ti_te = false;
     std::string displayTitleTerm;
@@ -268,12 +310,9 @@ public:
     bool activeImage = false;
 
     // hittory
-    bool UseHistory = true;
-    bool SaveURLHist = true;
     Hist *LoadHist = nullptr;
     Hist *SaveHist = nullptr;
     Hist *URLHist = nullptr;
-    int URLHistSize = 100;
     Hist *ShellHist = nullptr;
 
     // lineeditor
