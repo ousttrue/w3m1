17ea9940c34734aec9fdb5126016c247a5f67ea0
ousttrue <ousttrue@gmail.com>
2020-08-28 18:35:42 +0900

    loadHTMLStream

--- src/html/html_context.h
+++ src/html/html_context.h
@@ -1,11 +1,8 @@
 #pragma once
 #include <wc.h>
-#include "frontend/buffer.h"
-#include "stream/input_stream.h"
-#include "html/html.h"
-#include "html/form.h"
-#include "html/readbuffer.h"
-#include "html/table.h"
+#include <memory>
 
-
-std::shared_ptr<struct Buffer> loadHTMLStream(const URL &url, const InputStreamPtr &stream, CharacterEncodingScheme content_charset, bool internal = false);
+std::shared_ptr<struct Buffer> loadHTMLStream(const struct URL &url,
+                                              const std::shared_ptr<class InputStream> &stream,
+                                              CharacterEncodingScheme content_charset,
+                                              bool internal = false);
